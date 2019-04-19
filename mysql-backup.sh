a29e4f3d28fc605e831eff5183d9a75fc045dd46
Keith Lantz <klantz81@gmail.com>
2019-04-19 11:19:14 -0400

    fix to file extension

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -5,7 +5,7 @@ databases=`/usr/bin/mysql -NB --user=root --execute="SHOW DATABASES" | grep -Ev 
 for db in $databases
 do
 	/usr/bin/mysqldump --lock-tables --user=root --databases $db > "/root/mysql-backups/$db.sql"
-#	/usr/bin/mysqldump --lock-tables --user=root --databases $db | gzip > "/root/mysql-backups/$db.sql"
+#	/usr/bin/mysqldump --lock-tables --user=root --databases $db | gzip > "/root/mysql-backups/$db.sql.gz"
 #	/usr/bin/mysqldump --single-transaction --user=root --databases $db > "/root/mysql-backups/$db.sql"
 #	/usr/bin/mysqldump --single-transaction --user=root --databases $db | gzip > "/root/mysql-backups/$db.sql.gz"
 done
